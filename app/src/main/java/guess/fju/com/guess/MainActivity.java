cc2a4863857aa745de1f5c0f670a50afc21aadc4
Bonnie Chang <b22379222@gmail.com>
2017-11-07 20:36:44 +0800

    guess number app

--- app/src/main/java/guess/fju/com/guess/MainActivity.java
+++ app/src/main/java/guess/fju/com/guess/MainActivity.java
@@ -1,39 +1,40 @@
 package guess.fju.com.guess;
 
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    Random r = new Random();
-    int a= r.nextInt(10) + 1;
-
+    Random random = new Random();
+    int ram= random.nextInt(10) + 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
-
     public void guess(View view) {
-        EditText edNumber = (EditText) findViewById(R.id.b);
+        TextView r = (TextView)findViewById(R.id.t_n);
+        r.setText(Integer.toString(ram));
+        EditText edNumber = (EditText) findViewById(R.id.ed_number);
         int num = Integer.parseInt(edNumber.getText().toString());
-
-        if (a > num) {
-            Toast.makeText(this, "大一點",  Toast.LENGTH_LONG).show();
-        } else if (a < num) {
-            Toast.makeText(this, "小一點",  Toast.LENGTH_LONG).show();
+        TextView n = (TextView) findViewById(R.id.t_m);
+        if (ram > num) {
+            n.setText("大一點");
+ //           Toast.makeText(this, "大一點",  Toast.LENGTH_LONG).show();
+        } else if (ram < num) {
+            n.setText("小一點");
+ //           Toast.makeText(this, "小一點",  Toast.LENGTH_LONG).show();
         } else{
-            Toast.makeText(this, "答對了",  Toast.LENGTH_LONG).show();
+            n.setText("答對了");
+ /*           Toast.makeText(this, "答對了",  Toast.LENGTH_LONG).show();
         new AlertDialog.Builder(this)
                 .setMessage(Integer.toString(num))
                 .setPositiveButton("OK", null)
-                .show();
+                .show();     */
     }
 }
 }
