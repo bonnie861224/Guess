ac3d2d14f7f9b761872a4e9084b3b2a506e75e28
bonnie chang <b22379222@gmail.com>
2017-11-07 12:44:08 +0800

    ｔｅｓｔ

--- app/src/main/java/guess/fju/com/guess/MainActivity.java
+++ app/src/main/java/guess/fju/com/guess/MainActivity.java
@@ -5,10 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    Random r = new Random();
+    int a= r.nextInt(10) + 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,19 +19,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
-    public void number(View view){
-        EditText edNumber = (EditText) findViewById(R.id.b);
-        int number =Integer.parseInt(edNumber.getText().toString());
-        Random r=new Random();
-        int a,b;
-        a=r.nextInt(10)+1;
-        if (a>b)
-            System.out.println("大一點");
-        else if (a<b)
-            System.out.println("小一點");
-        else
-            System.out.println("答對了");
-        new AlertDialog.Builder(this).setPositiveButton("OK",null).show();
-    }
 
+    public void guess(View view) {
+        EditText edNumber = (EditText) findViewById(R.id.b);
+        int num = Integer.parseInt(edNumber.getText().toString());
+
+        if (a > num) {
+            Toast.makeText(this, "大一點",  Toast.LENGTH_LONG).show();
+        } else if (a < num) {
+            Toast.makeText(this, "小一點",  Toast.LENGTH_LONG).show();
+        } else{
+            Toast.makeText(this, "答對了",  Toast.LENGTH_LONG).show();
+        new AlertDialog.Builder(this)
+                .setMessage(Integer.toString(num))
+                .setPositiveButton("OK", null)
+                .show();
+    }
+}
 }
